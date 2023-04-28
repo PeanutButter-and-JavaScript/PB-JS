1c795b291bdcfa84a40e0d8478c82ac64ea6cf53
Daniel Quinn <dandidit.quinn@gmail.com>
2023-04-28 10:01:05 -0700

    src

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ function menuRender() {
     let div = document.createElement('div');
 
     let img = document.createElement('img');
-    img.src = PBJArray[i].src;
+    img.src = `../img/${PBJArray[i].src}.jpeg`;
     article.appendChild(img);
 
 
