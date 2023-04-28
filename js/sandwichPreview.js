7d4a6b87b464122d827cfd9b6b6999e55e75c705
Ashley Taylor <blueberrypea@gmail.com>
2023-04-28 11:19:52 -0400

    deleted sandy

--- js/sandwichPreview.js
+++ js/sandwichPreview.js
@@ -7,9 +7,12 @@ let lettuce = document.getElementById('lettuce');
 let tomato = document.getElementById('tomato');
 let bottomBun = document.getElementById('bottom-bun');
 let lettuceDown = document.getElementById('lettuce-down');
+
 //form elements
 let menuBread = document.getElementById('menuBread');
-
+let lettuceToss = document.getElementById('lettuceToss');
+let lettuceDownToss = document.getElementById('lettuceDownToss');
+let tomatoChange = document.getElementById('menuTomato')
 // add or remove multiple classes:
 //div.classList.add("foo", "bar", "baz");
 //div.classList.remove("foo", "bar", "baz");
@@ -48,8 +51,47 @@ function breadChanged(event) {
     bottomBun.classList.add("wheat")
   };
 };
+
+// lettuce.classList.add("grape");
+// lettuce.classList.add("strawberry");
+
+function lettuceChange(event) {
+  if (event.target.value === "grape") {
+    if (lettuce.classList.contains('strawberry')) {
+      bottomBun.classList.remove('strawberry');
+    }
+    lettuce.classList.add("grape");
+  }
+  else {
+    if (lettuce.classList.contains('grape')) {
+      lettuce.classList.remove('grape');
+    }
+    lettuce.classList.add("strawberry")
+  };
+};
+
+function lettuceDownChange(event) {
+  if (event.target.value === "grape") {
+    if (lettuceDown.classList.contains('strawberry')) {
+      bottomBun.classList.remove('strawberry');
+    }
+    lettuceDown.classList.add("grape");
+  }
+  else {
+    if (lettuceDown.classList.contains('grape')) {
+      lettuce.classList.remove('grape');
+    }
+    lettuceDown.classList.add("strawberry")
+  };
+};
 // Executable Code
 
 
-// Event Listeners
+// Event Listeners 
+// menu bread is the select element; matches the value at the top of the page ex. getelementbyid.
+//breadchange is the name of the function
 menuBread.addEventListener('change', breadChange);
+
+lettuceToss.addEventListener('change', lettuceChange);
+
+lettuceDownToss.addEventListener('change', lettuceDownChange);
